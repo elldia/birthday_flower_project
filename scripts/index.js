70a3a5db5cc0928f39b843d358030e57b0e8aef5
yuna <elldia1222@gmail.com>
2026-08-09 13:15:32 +0900

    js uploads

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,41 @@
-// index.js
-//3.위 입력칸(태어난 달을 숫자로 입력해주세요 input) 클릭 시 12 로 값 변경하기
-const user_birthday = document.querySelector('#user_birthday');
-console.log(user_birthday);
-
-//12 로 값 변경하기
-//객체.속성 = 값;
-function birthday12(){
-    user_birthday.value = 12;
-}
-
-//4. 나의 꽃과 꽃말 만나기 클릭 시 팝업창으로 당신의 탄생화는... 띄우고 배경 어둡게 만들기
-//(선행작업) 클릭 시 나타나려면 먼저 숨기기 필요
 const popup_wrap = document.querySelector('.popup_wrap');
 const popup_bg = document.querySelector('.popup_bg');
-console.log(popup_bg, popup_wrap);
+const user_birthdayInput = document.querySelector('#user_birthday');
+const birthday_result_btn = document.querySelector('#birthday_result_btn');
 
-//.popup_wrap {display:none;}
-//.popup_bg {display:none;}
-//자바스크립트에서 css쓰려면 작성하는 속성? 객체.style.속성 = 값
-popup_wrap.style.display = 'none';
-popup_bg.style.display = 'none';
-// 자바스크립트에서 display=none으로 숨겼다가 특정이벤트에 따라 다시 보이고 싶을때
-// 무조건 block을 쓰는게 아닌 그 선택자가 원래 가지고 있는 display속성으로 되돌리기
-// display:flex가 선택자에 원래 있었다면 자바스크립트도 display=flex 
-// display가 선택자에 없었다면? 원래 태그 고유속성에 따라 display=block 또는 display=inline
+//function start
+displayHide(popup_wrap);
+displayHide(popup_bg);
 
-function showPopup(){
-    popup_wrap.style.display = 'flex';
-    popup_bg.style.display = 'block';
-}
+//event
+birthday_result_btn.addEventListener('click',()=>{
+    if(user_birthdayInput.value == ''){ alert('숫자를 입력해주세요'); }
+    else if(user_birthdayInput.value == 1 || user_birthdayInput.value == 2 || user_birthdayInput.value == 3) {
+        displayShow(popup_wrap, 'flex');
+        displayShow(popup_bg);
+        popup_wrap.children[0].children[0].textContent = birthdayData[user_birthdayInput.value-1].month;
+        popup_wrap.children[1].children[0].textContent = birthdayData[user_birthdayInput.value-1].contents;
+        popup_wrap.children[2].children[1].textContent = birthdayData[user_birthdayInput.value-1].flower;
+    }else{ alert('현재는 1~3월 생일만 가능합니다. 빠른 시간안에 준비하도록 노력하겠습니다.');}
+})
+popup_bg.addEventListener('click',()=>{
+    displayHide(popup_bg); displayHide(popup_wrap);
+    user_birthdayInput.value = '';
+})
 
-//5. 배경 클릭 시 3번의 팝업창과 배경을 숨기기
-function hidePopup(){
-    popup_wrap.style.display = 'none';
-    popup_bg.style.display = 'none';
-}
+//function create
+function displayHide(target) {return target.style.display = 'none';}
+function displayShow(target, status='block') {target.style.display = status;}
+const birthdayData = [{
+    month:1,
+    flower:'장미',
+    contents:'장미의 뜻은..'
+},{
+    month:2,
+    flower:'수선화',
+    contents:'수선화의 뜻은..'
+},{
+    month:3,
+    flower:'민들레',
+    contents:'민들레의 뜻은..'
+}]
